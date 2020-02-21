e46e81d0379ed972a59baf9ba14d2dcf2f3dc0c0
tamcl <c.l.tam@lancaster.ac.uk>
2020-02-21 14:45:38 +0000

    go websites with ads

--- robo.java
+++ robo.java
@@ -22,29 +22,100 @@ public class robo
 
         // Create an instance of Robot class
         Robot robot = new Robot();
-	int tab = 10;
 
         // Press keys using robot. A gap of
         // of 500 mili seconds is added after
         // every key press
         while(true) {
             robot.keyPress(KeyEvent.VK_CONTROL);
-            
-		for(int i=0;i<tab;i++){
-		robot.keyPress(KeyEvent.VK_N);
-            	robot.keyRelease(KeyEvent.VK_N);
-Thread.sleep(500);
-		}
-		robot.keyRelease(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_N);
+        	robot.keyRelease(KeyEvent.VK_N);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+			Thread.sleep(1000);
+
+			type(robot);
+
             Thread.sleep(5000);
-	robot.keyPress(KeyEvent.VK_CONTROL);
-            for(int i=0;i<tab;i++){
-		robot.keyPress(KeyEvent.VK_W);
-            	robot.keyRelease(KeyEvent.VK_W);
-Thread.sleep(500);
-		}
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_W);
+            robot.keyRelease(KeyEvent.VK_W);
             robot.keyRelease(KeyEvent.VK_CONTROL);
+			Thread.sleep(100);
         }
 
     }
+
+	public static void type(Robot robot)throws IOException,
+            AWTException, InterruptedException{
+		robot.keyPress(KeyEvent.VK_E);
+		robot.keyRelease(KeyEvent.VK_E);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_Z);
+		robot.keyRelease(KeyEvent.VK_Z);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_G);
+		robot.keyRelease(KeyEvent.VK_G);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_I);
+		robot.keyRelease(KeyEvent.VK_I);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_F);
+		robot.keyRelease(KeyEvent.VK_F);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_PERIOD);
+		robot.keyRelease(KeyEvent.VK_PERIOD);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_C);
+		robot.keyRelease(KeyEvent.VK_C);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_O);
+		robot.keyRelease(KeyEvent.VK_O);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_M);
+		robot.keyRelease(KeyEvent.VK_M);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_SLASH);
+		robot.keyRelease(KeyEvent.VK_SLASH);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_V);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_I);
+		robot.keyRelease(KeyEvent.VK_I);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_D);
+		robot.keyRelease(KeyEvent.VK_D);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_E);
+		robot.keyRelease(KeyEvent.VK_E);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_O);
+		robot.keyRelease(KeyEvent.VK_O);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_MINUS);
+		robot.keyRelease(KeyEvent.VK_MINUS);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_T);
+		robot.keyRelease(KeyEvent.VK_T);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_O);
+		robot.keyRelease(KeyEvent.VK_O);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_MINUS);
+		robot.keyRelease(KeyEvent.VK_MINUS);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_G);
+		robot.keyRelease(KeyEvent.VK_G);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_I);
+		robot.keyRelease(KeyEvent.VK_I);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_F);
+		robot.keyRelease(KeyEvent.VK_F);
+		Thread.sleep(100);
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(100);
+		
+	}
 }
